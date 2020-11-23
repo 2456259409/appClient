7fa64aaac5cc5c3c1d019890ab69108e2fc8b70c
任建 <5210249+rj2456259409@user.noreply.gitee.com>
2020-11-23 22:57:02 +0800

    提交

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,29 @@
 <template>
-	<view class="content">
+	<view>
+		<view class="cu-bar tabbar margin-bottom-xl bg-white my-bottom">
+			<view class="action text-green">
+				<view class="cuIcon-homefill"></view> 首页
+			</view>
+			<view class="action text-gray">
+				<view class="cuIcon-similar"></view> 分类
+			</view>
+			<view class="action text-gray add-action">
+				<button class="cu-btn cuIcon-add bg-green shadow"></button>
+				发布
+			</view>
+			<view class="action text-gray">
+				<view class="cuIcon-cart">
+					<view class="cu-tag badge">99</view>
+				</view>
+				购物车
+			</view>
+			<view class="action text-gray">
+				<view class="cuIcon-my">
+					<view class="cu-tag badge"></view>
+				</view>
+				我的
+			</view>
+		</view>
 		
 	</view>
 </template>
@@ -8,18 +32,23 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
+				InputBottom: 0
+			};
 		},
 		methods: {
-
+		
 		}
 	}
 </script>
 
 <style>
-
+page{
+  padding-bottom: 100upx;
+}
+.my-bottom{
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	right: 0;
+}
 </style>
